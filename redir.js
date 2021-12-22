f2a7e80ee2fd6d03bb64fdc7cbf898d0cb2739da
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2021-12-22 22:27:06 +0700

    Create redir.js

---
+++ redir.js
@@ -0,0 +1,5 @@
+var d='';
+var d= window.location.href;
+d=d.replace("?", " ");
+d=d.match(/\d{11,20}/);
+location.href = 'http://s.click.aliexpress.com/deep_link.htm?dl_target_url=http://www.aliexpress.com/item/item/'+d+'.html';
